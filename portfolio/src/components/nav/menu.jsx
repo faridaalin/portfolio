60790d9bc9c3ce59c0dea3e77f76b9efbf464bbd
Farida <farida.alin@yahoo.com>
2020-08-06 18:24:52 +0200

    changed header styling to styled component

--- portfolio/src/components/nav/menu.jsx
+++ portfolio/src/components/nav/menu.jsx
@@ -5,21 +5,52 @@ import LogoContainer from './Logo-container'
 import AsideMenu from './asideMenu'
 import Hamburger from './hamburger'
 
+import styled from 'styled-components';
+
+const StyledContainer = styled.header`
+background-color: #f2f2f2;
+display: flex;
+flex-direction: row;
+padding: 32px 32px;
+justify-content: space-between;
+align-items: center;
+position: fixed;
+top: 0;
+left: 0;
+right: 0;
+z-index: 2000;
+transform: translateY(${props => props.scrollDirection === 'down' ? '-88px' : '0px'});
+box-shadow: ${props => props.scrollDirection === 'up' ? `rgb(160 160 160 / 70%) 0px 10px 30px -19px` : 'none'};
+`
+
+
+
 const Menu = ({ location }) => {
     const [open, setOpen] = useState(false);
     const headerRef = useRef(null)
 
-    const [isFixed, setFixed] = useState(false)
+    const [scrollDirection, setscrollDirection] = useState('none')
+
     let previousScroll = 0;
 
-
     const handleScroll = () => {
-        if(window.scrollY > previousScroll) { //true
-            setFixed(window.scrollY > previousScroll)
-        } else{
-            setFixed(window.scrollY > previousScroll) //false
+        const fromTop = window.scrollY; 
+
+        if(fromTop < 5) {
+            setscrollDirection('none')
+            console.log('none')
+        } else if(fromTop > previousScroll && fromTop > headerRef.current.offsetHeight) {
+            if(scrollDirection !== 'down') {
+                setscrollDirection('down')
+            }
+        } else if(fromTop < previousScroll) {
+            if (scrollDirection !== 'up') {
+                setscrollDirection('up')
+            }
         }
-        previousScroll = window.scrollY
+
+        previousScroll = fromTop;
+
     }
 
     useEffect(() => {
@@ -29,15 +60,17 @@ const Menu = ({ location }) => {
         }
     }, [])
 
+
+
     return (
 
-        <header className={`header ${isFixed ? 'hidden' : 'fixed-nav'}`} ref={headerRef}>
+        <StyledContainer scrollDirection={scrollDirection}  ref={headerRef}>
             <LogoContainer />
             {location.pathname !== '/' && location.pathname !== '/home' && <nav>
                 <Hamburger open={open} setOpen={setOpen} />
                 <AsideMenu setOpen={setOpen} open={open} />
             </nav>}
-        </header>
+        </StyledContainer>
 
     )
 }
