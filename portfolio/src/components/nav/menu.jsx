63023688a341b1f02fa5bad8038eabd595058c1e
Farida <farida.alin@yahoo.com>
2020-08-08 16:31:54 +0200

    added // eslint-disable-next-line to useEffect Hook

--- portfolio/src/components/nav/menu.jsx
+++ portfolio/src/components/nav/menu.jsx
@@ -66,6 +66,7 @@ const Menu = ({ location }) => {
         return () => {
             window.removeEventListener('scroll', () => handleScroll)
         }
+        // eslint-disable-next-line
     }, [])
 
 
