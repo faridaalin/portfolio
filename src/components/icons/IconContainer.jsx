3f0ed427a6d5664294587dda6d6fceaa96ce05f9
Farida <farida.alin@yahoo.com>
2020-10-13 09:09:23 +0200

    removed imported router

--- src/components/icons/IconContainer.jsx
+++ src/components/icons/IconContainer.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import { LinkedIn, Email, Github } from './Icons'
+import { LinkedIn, Github } from './Icons'
 
 function IconContainer() {
     return (
         <div className="icon-container">
             <div className="icon-container-inner">
-            <LinkedIn link="https://www.linkedin.com/in/farida-a-"/>
-            <Github link="https://github.com/faridaalin/"/>
-            <Email link="mailto: faridaalin8@gmail.com"/>
+                <LinkedIn link="https://www.linkedin.com/in/farida-a-" />
+                <Github link="https://github.com/faridaalin/" />
             </div>
         </div>
     )
